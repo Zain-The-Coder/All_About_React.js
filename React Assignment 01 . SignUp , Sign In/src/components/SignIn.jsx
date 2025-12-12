f51e3ca509e57dd56c58b52ba641ad2a74d881d3
Zain-The-Coder <zain015976@gmail.com>
2025-12-13 03:20:53 +0500

    React Assignment

--- React Assignment 01 . SignUp , Sign In/src/components/SignIn.jsx
+++ React Assignment 01 . SignUp , Sign In/src/components/SignIn.jsx
@@ -1,38 +1,65 @@
+import { useState } from "react";
+
+function SignIn() {
+  const [error, setError] = useState("");
+  const [userName, setuserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const errorHandler = () => {
+    if (!userName || !userEmail || !userPassword) {
+      setError("Please Fill The Above All Fields !");
+      return;
+    }
+
+    if (userPassword.length < 6) {
+      setError("Password Is Too Short");
+      return;
+    }
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    errorHandler();
+    setError("Login Successfully !");
+      setuserName("");
+      setUserEmail("");
+      setUserPassword("");
+  }
 
 
-
-
-
-function SignIn () {
-    return (
-        <>
+  return (
+    <>
       <section className="min-h-screen flex items-center justify-center bg-gray-100 p-[40px] w-[100%]">
         <div className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-md">
-        <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">Sign In Form</h1>
+          <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">Sign In Form</h1>
 
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={formSubmit}>
             <div className="flex flex-col text-left">
               <label htmlFor="userName" className="font-semibold text-gray-700">Enter Your Name</label>
-              <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400" type="text" id="userName"/>
+              <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400" type="text" id="userName"value={userName}
+                onChange={(e) => {setuserName(e.target.value);}}/>
             </div>
 
             <div className="flex flex-col text-left">
               <label htmlFor="userEmail" className="font-semibold text-gray-700">Enter Your Email</label>
-                <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400" type="email" id="userEmail"/>
+              <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"type="email"id="userEmail"value={userEmail}
+                onChange={(e) => {setUserEmail(e.target.value);}}/>
             </div>
 
             <div className="flex flex-col text-left">
               <label htmlFor="userPassword" className="font-semibold text-gray-700">Enter Your Password</label>
-              <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400" type="password" id="userPassword"/>
+              <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400" type="password" id="userPassword" value={userPassword}
+               onChange={(e) => {setUserPassword(e.target.value);}}/>
             </div>
 
-            <p></p>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-xl font-semibold transition-all mt-3" type="submit">Sign Up</button>
-        </form> 
+            <p className="text-black italic text-[17px]">{error}</p>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-xl font-semibold transition-all mt-3 cursor-pointer" type="submit">Sign Up</button>
+          </form>
         </div>
       </section>
     </>
   );
 }
 
-export default SignIn ;
+export default SignIn;
--- React Assignment 01 . SignUp , Sign In/src/components/SignUp.jsx
+++ React Assignment 01 . SignUp , Sign In/src/components/SignUp.jsx
@@ -1,61 +1,92 @@
 import { useState } from "react";
+function SignUp() {
+    const [error, setError] = useState("");
+    const [userName, setUserName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [userPassword, setUserPassword] = useState("");
 
-function SignUp () {
-    const [error , setError] = useState("");
-    const [userName , setUserName] = useState("");
-    const [userEmail , setUserEmail] = useState("");
-    const [userPassword , setUserPassword] = useState("");
+    let userKnames = [];
+    let userKemails = [];
+    let userKpasswords = [];
 
-    
-    function formSubmit (e) {
-        let x = userPassword.length;
-        e.preventDefault();
-        
-        if(!userName || !userEmail || !userPassword) {
-            setError("Fill The Above All Fields !");
-            return;
+    function errorHandler() {
+        if (!userName || !userEmail || !userPassword) {
+            setError("Fill The Above All Requirements !");
+            return false;
         }
-        if(x < 6) {
-            setError("Password Is Too Short !");
-            return ;
+        if (!userEmail.includes("@")) {
+            setError("Please Enter Correct Email !");
+            return false;
         }
-        setError("Form Submitted Successfully !");
-        let userInfo = {
-            nameOfUser : userName , 
-            emailOfUser : userEmail , 
-            passwordOfUser : userPassword ,
+        if (userEmail.includes(" ")) {
+            setError("Remove Extra Spaces From Email");
+            return false;
         }
-
+        if (userPassword.length < 6) {
+            setError("Password Should Be Strong or 6 Letters !");
+            return false;
+        }
+        setError("Login Successfully !");
+        return true;
     }
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+        if (!errorHandler()) return;
+
+        let x = localStorage.getItem("userNames");
+        let y = localStorage.getItem("userEmails");
+        let z = localStorage.getItem("userPasswords");
+
+        if (x) userKnames = JSON.parse(x);
+        if (y) userKemails = JSON.parse(y);
+        if (z) userKpasswords = JSON.parse(z);
+
+        userKnames.push(userName);
+        userKemails.push(userEmail);
+        userKpasswords.push(userPassword);
+
+        localStorage.setItem("userNames", JSON.stringify(userKnames));
+        localStorage.setItem("userEmails", JSON.stringify(userKemails));
+        localStorage.setItem("userPasswords", JSON.stringify(userKpasswords));
+
+        setUserName("");
+        setUserEmail("");
+        setUserPassword("");
+    };
 
     return (
         <>
         <section className="min-h-screen flex items-center justify-center bg-gray-100 p-[40px] w-[100%]">
             <div className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-md">
-            
                 <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">Sign Up Form</h1>
 
                 <form className="flex flex-col gap-4" onSubmit={formSubmit}>
-                    
                     <div className="flex flex-col text-left">
                         <label htmlFor="userName" className="font-semibold text-gray-700">Enter Your Name</label>
-                        <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400" type="text" id="userName" onChange={(e) => {setUserEmail(e.target.value)}}/>
+                        <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                            type="text" id="userName" value={userName}
+                            onChange={(e) => setUserName(e.target.value)} />
                     </div>
 
                     <div className="flex flex-col text-left">
                         <label htmlFor="userEmail" className="font-semibold text-gray-700">Enter Your Email</label>
-                        <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400" type="email" id="userEmail" onChange={(e) => {setUserPassword(e.target.value)}} />
+                        <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                            type="email" id="userEmail" value={userEmail}
+                            onChange={(e) => setUserEmail(e.target.value)} />
                     </div>
 
                     <div className="flex flex-col text-left">
                         <label htmlFor="userPassword" className="font-semibold text-gray-700">Enter Your Password</label>
-                        <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400" type="password" id="userPassword" onChange={(e) => {setUserPassword(e.target.value)} }/>
+                        <input className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                            type="password" id="userPassword" value={userPassword}
+                            onChange={(e) => setUserPassword(e.target.value)} />
                     </div>
 
-                    <p className="text-red-500 font-semibold">{error}</p>
+                    <p className="text-black italic text-[17px]">{error}</p>
 
                     <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-xl font-semibold transition-all mt-3 cursor-pointer" type="submit">Sign Up</button>
-                </form> 
+                </form>
             </div>
         </section>
         </>
@@ -63,3 +94,4 @@ function SignUp () {
 }
 
 export default SignUp;
+
