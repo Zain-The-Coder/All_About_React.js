b8287cf4ccdea269ffb437bddd5a7b3cc1bb1aaf
Zain-The-Coder <zain015976@gmail.com>
2025-12-15 01:51:48 +0500

    React JS

--- Learning React Hooks/src/App.jsx
+++ Learning React Hooks/src/App.jsx
@@ -5,29 +5,37 @@ import { useEffect, useState } from 'react';
 
 function App () {
 
-    let [state , setState] = useState(false);
+  // :Learning : What Is Props Drilling ???
+    
 
-    const changeState = () => {
-        setState(true);
-    }
 
-  let myTimeOut = useEffect(() => {
-    console.log("Effected...");
-    let myTimeOut = setTimeout(() => {
-      setState(false);
-    }, 5000);
-    return () => clearTimeout(myTimeOut);
-  }, [state]);
 
-    return (
-        <>
-        <h1 className='text-center text-[30px] italic uppercase'>Hello React</h1>
-        <button onClick={changeState} className='border px-[20px] py-[3px] rounded-2xl cursor-pointer'>Show Message</button>
-        {state && (<div className="bg-green-300 absolute right-5 top-20 text-green-800 text-2xl p-5 rounded-2xl">Hello World
-          </div>
-)}
-        </>
-    )
+
+
+  // let [press , setPress] = useState(false);
+
+  // const buttonPressed = () => {
+  //   setPress(true);
+  // }
+
+  // useEffect(() => {
+  //   if(press){
+  //   console.log("run")
+  //   let myTimeout = setTimeout(() => {
+  //     setPress(false);
+  //   } , 3000);
+  //   return () => clearTimeout(myTimeout);
+  // }
+  // }, [press]);
+
+  // return (
+  //   <>
+  //   <h1 className='text-center uppercase text-[28px]'>Hello World</h1>
+  //   {press && (<p className='w-[40%] m-auto p-[20px] text-[20px] italic shadow-xl mt-[30px] rounded-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, repellendus laudantium voluptatum ipsum esse quo! Quae officia quasi iste eius dolore earum. Provident, laborum ex. Ducimus magnam quod ab sunt?</p>)}
+  //   <button onClick={buttonPressed} className='border rounded px-[10px] block m-auto mt-[20px] cursor-pointer'>Show Message</button>
+
+  //   </>
+  // )
 
 }
 export default App
